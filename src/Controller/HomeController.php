0f4dccf1c765f75869a0b84c35510f9da29d67a1
Vanessa FAYARD <vanessa.fayard@gmail.com>
2019-01-22 09:13:52 +0100

    "Homepage and best ads/users"

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     *
+     * @param AdRepository $adRepository
+     * @param UserRepository $userRepository
+     * @return Response
      */
-    public function home()
+    public function home(AdRepository $adRepository, UserRepository $userRepository)
     {
-        return $this->render('home/home.html.twig');
+        return $this->render(
+            'home/home.html.twig',
+            [
+                'ads' => $adRepository->findBestAds(3),
+                'users' => $userRepository->findBestUsers(2)
+            ]
+        );
     }
 }
